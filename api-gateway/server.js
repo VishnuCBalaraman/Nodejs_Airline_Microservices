d13e9c4348c04d8c78e58283eddf85ad4488f1bd
VishnuCBalaraman <vishnucbalaram123@gmail.com>
2024-10-16 17:25:57 +0530

    First commit

---
+++ api-gateway/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = 4000;
+
+app.use('/airlines', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/flights', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/passengers', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+
+app.listen(PORT, () => {
+  console.log(`API Gateway running on port ${PORT}`);
+});
